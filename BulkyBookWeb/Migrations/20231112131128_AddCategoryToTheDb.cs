11629b43ac58ddd779a3e40a22b0468d3fe62b7d
ngosakotati <140051754+ngosakotati@users.noreply.github.com>
2023-11-15 11:41:57 +0200

    All

--- BulkyBookWeb/Migrations/20231112131128_AddCategoryToTheDb.cs
+++ BulkyBookWeb/Migrations/20231112131128_AddCategoryToTheDb.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace BulkyBookWeb.Migrations
 {
     /// <inheritdoc />
-    public partial class AddCategoryToDatabase : Migration
+    public partial class AddCategoryToTheDb : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
